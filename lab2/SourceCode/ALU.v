53ee04a4baefa07c591d1788ca3cf90e49c03fb1
wildoranges <zkd18cjb@mail.ustc.edu.cn>
2021-04-19 20:59:50 +0800

    ALU design

--- lab2/SourceCode/ALU.v
+++ lab2/SourceCode/ALU.v
@@ -28,9 +28,49 @@ module ALU(
     input wire [31:0] Operand2,
     input wire [3:0] AluContrl,
     output reg [31:0] AluOut
-    );    
-    
-    // 请补全此处代码
-
+);    
+always @(*) 
+begin
+    case (AluContrl)
+        `SLL:       AluOut <= Operand1 <<  Operand2;
+        `SRL:       AluOut <= Operand1 >>  Operand2;
+        `SRA:       AluOut <= Operand1 >>> Operand2;
+        `ADD:       AluOut <= Operand1  +  Operand2;
+        `SUB:       AluOut <= Operand1  -  Operand2;
+        `XOR:       AluOut <= Operand1  ^  Operand2;
+        `OR:        AluOut <= Operand1  |  Operand2;
+        `AND:       AluOut <= Operand1  &  Operand2;
+        `SLTU:       begin
+            if(Operand1 < Operand2)
+                AluOut <= 32'd1;
+            else
+                AluOut <= 32'd0;
+        end
+        `SLT:      begin
+            case ({Operand1[31],Operand2[31]})
+                /*2'b00:begin
+                    if(Operand1 < Operand2)
+                        AluOut <= 32'd1;
+                    else
+                        AluOut <= 32'd0;
+                end*/
+                2'b01:begin
+                    AluOut <= 32'd0;
+                end
+                2'b10:begin
+                    AluOut <= 32'd1;
+                end
+                default:begin
+                    if(Operand1 < Operand2)
+                        AluOut <= 32'd1;
+                    else
+                        AluOut <= 32'd0; 
+                end
+            endcase
+        end
+        `LUI:   AluOut <= {Operand2[31:12],12'd0};
+        default:    ALUout <= 32'h0000_0000;
+    endcase
+end
 endmodule
 
