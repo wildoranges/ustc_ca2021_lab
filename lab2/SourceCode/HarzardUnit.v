9aa9f2ae18e5bdcd21f2f98c9585d80fd32704af
wildoranges <zkd18cjb@mail.ustc.edu.cn>
2021-04-21 12:15:07 +0800

    modify hazard

--- lab2/SourceCode/HarzardUnit.v
+++ lab2/SourceCode/HarzardUnit.v
@@ -10,16 +10,16 @@
 // Description: Deal with harzards in pipline
 //////////////////////////////////////////////////////////////////////////////////
 //功能说明
-    //HarzardUnit用来处理流水线冲突，通过插入气泡，forward以及冲刷流水段解决数据相关和控制相关，组合逻辑电路
-    //可以最后实现。前期测试CPU正确性时，可以在每两条指令间插入四条空指令，然后直接把本模块输出定为，不forward，不stall，不flush 
+    //HarzardUnit用来处理流水线冲突，通过插入气泡，forward以及冲刷流水段解决数据相关和控制相关，组合�?�辑电路
+    //可以�?后实现�?�前期测试CPU正确性时，可以在每两条指令间插入四条空指令，然后直接把本模块输出定为，不forward，不stall，不flush 
 //输入
-    //CpuRst                                    外部信号，用来初始化CPU，当CpuRst==1时CPU全局复位清零（所有段寄存器flush），Cpu_Rst==0时cpu开始执行指令
+    //CpuRst                                    外部信号，用来初始化CPU，当CpuRst==1时CPU全局复位清零（所有段寄存器flush），Cpu_Rst==0时cpu�?始执行指�?
     //ICacheMiss, DCacheMiss                    为后续实验预留信号，暂时可以无视，用来处理cache miss
     //BranchE, JalrE, JalD                      用来处理控制相关
-    //Rs1D, Rs2D, Rs1E, Rs2E, RdE, RdM, RdW     用来处理数据相关，分别表示源寄存器1号码，源寄存器2号码，目标寄存器号码
-    //RegReadE RegReadD[1]==1                   表示A1对应的寄存器值被使用到了，RegReadD[0]==1表示A2对应的寄存器值被使用到了，用于forward的处理
-    //RegWriteM, RegWriteW                      用来处理数据相关，RegWrite!=3'b0说明对目标寄存器有写入操作
-    //MemToRegE                                 表示Ex段当前指令 从Data Memory中加载数据到寄存器中
+    //Rs1D, Rs2D, Rs1E, Rs2E, RdE, RdM, RdW     用来处理数据相关，分别表示源寄存�?1号码，源寄存�?2号码，目标寄存器号码
+    //RegReadE RegReadD[1]==1                   表示A1对应的寄存器值被使用到了，RegReadD[0]==1表示A2对应的寄存器值被使用到了，用于forward的处�?
+    //RegWriteM, RegWriteW                      用来处理数据相关，RegWrite!=3'b0说明对目标寄存器有写入操�?
+    //MemToRegE                                 表示Ex段当前指�? 从Data Memory中加载数据到寄存器中
 //输出
     //StallF, FlushF, StallD, FlushD, StallE, FlushE, StallM, FlushM, StallW, FlushW    控制五个段寄存器进行stall（维持状态不变）和flush（清零）
     //Forward1E, Forward2E                                                              控制forward
@@ -40,9 +40,18 @@ module HarzardUnit(
     
 always@(*)
 begin
+    if(CpuRst)
+    begin
+    {StallF, FlushF, StallD, FlushD, StallE, FlushE, StallM, FlushM, StallW, FlushW} <= 10'b0101010101;
+    Forward1E <= 2'b00;
+    Forward2E <= 2'b00;
+    end
+    else
+    begin
     {StallF, FlushF, StallD, FlushD, StallE, FlushE, StallM, FlushM, StallW, FlushW} <= 10'd0;
     Forward1E <= 2'b00;
     Forward2E <= 2'b00;
+    end
 end
 
 endmodule
